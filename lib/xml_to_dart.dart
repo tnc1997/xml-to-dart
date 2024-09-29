c4ff02ec6969fde4a123e2d5480d569f9fcd0fa0
Thomas Clark <tnc1997@outlook.com>
2024-09-29 09:51:40 +0100

    refactor: add xml to dart builder

---
+++ lib/xml_to_dart.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:build/build.dart';
+
+class XmlToDartBuilder implements Builder {
+  @override
+  final buildExtensions = const {
+    '.xml': [
+      '.dart',
+    ],
+  };
+
+  const XmlToDartBuilder();
+
+  @override
+  Future<void> build(
+    BuildStep buildStep,
+  ) {
+    throw UnimplementedError();
+  }
+}

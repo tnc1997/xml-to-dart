4f6af58865685ce1c1a293cf80afefdb3ebfd52b
Thomas Clark <tnc1997@outlook.com>
2024-10-12 14:24:19 +0100

    refactor: add type arguments to dart type

--- lib/src/dart_type.dart
+++ lib/src/dart_type.dart
@@ -1,10 +1,12 @@
 class DartType {
   final String name;
   final NullabilitySuffix nullabilitySuffix;
+  final List<DartType> typeArguments;
 
   const DartType({
     required this.name,
     this.nullabilitySuffix = NullabilitySuffix.none,
+    this.typeArguments = const [],
   });
 
   factory DartType.fromValue(
@@ -36,6 +38,20 @@ class DartType {
   @override
   String toString() {
     final buffer = StringBuffer(name);
+
+    if (typeArguments.isNotEmpty) {
+      buffer.write('<');
+
+      for (var i = 0; i < typeArguments.length; i++) {
+        buffer.write(typeArguments[i]);
+
+        if (i < typeArguments.length - 1) {
+          buffer.write(', ');
+        }
+      }
+
+      buffer.write('>');
+    }
 
     if (nullabilitySuffix == NullabilitySuffix.question) {
       buffer.write('?');
